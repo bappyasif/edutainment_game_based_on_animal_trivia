660d64cafb1bba733d54f77f96f74fd3f96f412a
Lemon <riegoshinandrei@gmail.com>
2021-07-20 11:27:33 +0800

    Added non-responsive ending screen layout

--- webpack.config.js
+++ webpack.config.js
@@ -4,20 +4,20 @@ module.exports = {
     entry: './src/index.js',
     output: {
         filename: `[name].bundle.js`,
-        path: path.resolve(__dirname, 'dist')
+        path: path.resolve(__dirname, 'dist'),
     },
     mode: 'development',
     devServer: {
         contentBase: path.join(__dirname, 'dist'),
         compress: true,
-        hotOnly: true
+        hotOnly: true,
     },
     module: {
         rules: [
             {
                 test: /\.(png|jpg|jpeg|gif|svg)$/i,
-                type: 'asset/resources'
-            }
-        ]
-    }
-}
+                type: 'asset/resources',
+            },
+        ],
+    },
+};
