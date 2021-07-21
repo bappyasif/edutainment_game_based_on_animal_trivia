8adaee35fe2b70fb1e4c6cbf3300ba9da8283660
Lemon <riegoshinandrei@gmail.com>
2021-07-21 12:18:39 +0800

    Added randomizer

---
+++ src/randomizer.js
@@ -0,0 +1,34 @@
+const data = {
+    mammals: [
+        {
+            question: 'test',
+            answer: true,
+            explanation: 'this is a test',
+        },
+    ],
+};
+
+const getQuestions = (category) => {
+    const categoryData = data[category];
+    let prevQuestions = [];
+
+    const nextQuestion = () => {
+        // if we run out of questions, return undefined
+        if (prevQuestions.length === categoryData.length) return;
+
+        let idx;
+        do {
+            idx = Math.floor(Math.random() * categoryData.length);
+        } while (prevQuestions.includes(idx));
+        prevQuestions.push(idx);
+
+        return categoryData[idx];
+    };
+
+    const reset = () => {
+        prevQuestions = [];
+    };
+
+    // returns a question iterator
+    return { reset, next: nextQuestion };
+};
