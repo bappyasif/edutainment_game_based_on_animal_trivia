0e928586a08b0eaca201f392425eeac518bae84b
bappyasif <bappyasif@hotmail.com>
2021-07-23 22:18:15 +0600

    now it has fixes for import/export configuration for jest

--- jest-codes/randomizer/index.test.js
+++ jest-codes/randomizer/index.test.js
@@ -2,5 +2,6 @@ const { test, expect } = require("@jest/globals");
 const { getQuestions } = require("../../src/randomizer");
 
 test('checking randomizer function intricacies', () => {
-    expect(getQuestions('mammals').next()).toBe(null);
+    // expect(getQuestions('mammals').next()).toBe(null);
+    expect(getQuestions('mammals').next()).not.toBe(null);
 });
