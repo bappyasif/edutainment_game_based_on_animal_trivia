71a2937464812fd759d9453e85c2e63a639ae9ea
Lemon <riegoshinandrei@gmail.com>
2021-08-01 18:29:46 +0800

    Fix typo

--- src/all-layouts/ChooseDifficulty.js
+++ src/all-layouts/ChooseDifficulty.js
@@ -2,7 +2,7 @@ const ChooseDifficulty = () =>
     document.createRange().createContextualFragment(
         `<div class="modal" style="--modal-width: 40%; --modal-my: 10%;">
             <div class="modal__container container--vertical">
-                <p class="modal__title">Choose diffuculty</p>
+                <p class="modal__title">Choose difficulty</p>
                 <div class="difficulty container--vertical" id="difficulty">
                     <button data-value="easy" class="difficulty__btn btn--secondary">Easy</button>
                     <button data-value="hard" class="difficulty__btn btn--secondary">Hard</button>
